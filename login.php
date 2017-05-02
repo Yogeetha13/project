2db98efd0265cb64e9c24064695d3f7b820dfe43
yogeetha susparsha <ys467@afsconnect1.njit.edu>
2017-05-02 15:55:46 -0400

    login

---
+++ login.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+ <h1> Login page</h1>
+   <form method = "post" action="index.php">
+	   <h2>Username:</h2><input type="text" name="name"/><br><br>
+  	   <h2>Password:</h2><input type="password" name="password"/><br><br>
+  	   <input type ="hidden" name="action" value="user"><br>
+   	   <input type="submit" value="Login"/>
+   </form>
+</body	
+</html>
