d5c2867aa67b6710edda4c37342d13a54f6d2421
yogeetha susparsha <ys467@afsconnect1.njit.edu>
2017-05-03 13:54:30 -0400

    todoItem an create user

--- db1.php
+++ db1.php
@@ -1,4 +1,50 @@
 <?php
+function addTodoItems($uid,$todo_text)
+{
+	global $db;
+	$query = 'insert into todos(user_id,todo_item) values(:userid,:todo_text)';
+	$statement = $db->prepare($query);
+	$statement->bindValue(':userid',$uid);
+	$statement->bindValue(':todo_text',$todo_text);
+	$statement->execute();
+	$statement->closeCursor();
+}
+function getTodoItems($uid)
+{
+	global $db;
+	$query = 'select * from todos where user_id= :userid';
+	$statement = $db->prepare($query);
+	$statement->bindValue(':userid',$uid);
+	$statement->execute();
+	$result= $statement->fetchAll();
+	$statement->closeCursor();
+	return $result;
+}
+function createUser($username, $password)
+{
+	global $db;
+	$query = 'select * from users where username = :name ';
+	$statement = $db->prepare($query);
+	$statement->bindValue(':name',$username);
+	$statement->execute();
+	$result= $statement->fetchAll();
+	$statement->closeCursor();
+	$count = $statement->rowCount();
+	if($count > 0 )
+	{
+		return true;
+	}
+	else
+	{
+		$query = 'insert into users(username,passwordHash) values (:name, :pass)';
+		$statement = $db->prepare($query);
+		$statement->bindValue(':name',$username);
+		$statement->bindValue(':pass',$password);
+		$statement->execute();
+		$statement->closeCursor();
+		return false;
+	}
+}
    function isUserValid($username,$password)
    {
    	global $db;
