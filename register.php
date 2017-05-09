289eaa3286126e37e48f1a95503c5f67b24d1f3a
yogeetha susparsha <ys467@afsconnect1.njit.edu>
2017-05-08 22:21:03 -0400

    final commit

--- register.php
+++ register.php
@@ -1,22 +1,42 @@
 <html>
+<head>
+<link rel="stylesheet" type="text/css" href="login.css">
+</head>
 <body>
+<div class="wrapper">
+<div class="container">
   <h1> Sign Up</h1>
   <form method = 'post' action = 'index.php'>
-       <strong> First Name: </strong> <input type = "text" name = 'fname'
+       <input type = "text" name = 'fname'
        placeholder="firstname"/> <br> </br>
-       <strong> Last Name: </strong> <input type= "text" name = 'lname'
+       <input type= "text" name = 'lname'
        placeholder="lastname" /> <br> </br>
-       	<strong> Email Address: </strong> <input type = "text" name='email'
+       <input type = "text" name='email'
 	placeholder = "email address"/> <br> </br>
-	<strong> Password: </strong> <input type="password"
+	<input type="password"
 	name='password'value="" placeholder = "password" /> <br> </br>
-	<strong> Phone number: </strong> <input type="int"
+	<input type="int"
 	name='ph_number'value="" placeholder = "phone number" /> <br> </br>
-	<strong> Birthday: </strong> <input type="date" name='bday'/> <br> </br>
-	<strong> Gender: </strong> <input type="text" name='gender'/> <br> </br>
+	<input type="date" name='bday'/> <br> </br>
+	<input type="text" name='gender' placeholder="gender"/> <br> </br>
 	<input type="hidden" name= "action" value="register">
 	<br>
-	<input type="submit" value="Register"/>
+	<input type="submit" value="Register" id="login-button"/>
   </form>
+  </div>
+  <ul class="bg-bubbles">
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  </ul>
+  </div>
   </body>
   </html>
